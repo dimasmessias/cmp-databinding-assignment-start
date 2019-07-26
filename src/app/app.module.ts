c32edb098180c950e5ad8bff11d38e49d1c20bf3
Dimas Messias <dimas_messias@hotmail.com>
2019-07-26 20:34:17 +0200

    Adde bootstrap file and fixed wrong component name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { GameControlComponent } from './game/game/control/game-control.component';
-import { OddComponent } from './game/odd/odd.component';
+import { GameControlComponent } from './game/control/game-control.component';
 import { EvenComponent } from './game/even/even.component';
+import { OddComponent } from './game/odd/odd.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    GameControlComponent,
-    OddComponent,
-    EvenComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		GameControlComponent,
+		OddComponent,
+		EvenComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule {}
