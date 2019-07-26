7ef820ad7865b2f4335531fd4368465a8b63895e
Dimas Messias <dimas_messias@hotmail.com>
2019-07-26 20:26:25 +0200

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { GameControlComponent } from './game/game/control/game-control.component';
+import { OddComponent } from './game/odd/odd.component';
+import { EvenComponent } from './game/even/even.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GameControlComponent,
+    OddComponent,
+    EvenComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
